ab414cac3d7f3ae77c410ab8e7bff433daf5534a
ermeng-sspc <2444622785@qq.com>
2024-07-31 13:52:39 +0800

    更新 externel/api.py 中bot_names 生成逻辑，使用配置文件获取 bot 名称，确保包含 "Assistant"

--- external/api.py
+++ external/api.py
@@ -22,10 +22,17 @@ app = FastAPI()
 
 client_dict = {}
 
-bot_names = {"Assistant", "ChatGPT-16k", "GPT-4", "GPT-4o", "GPT-4o-Mini", "GPT-4-128k", "Claude-3-Opus", "Claude-3.5-Sonnet",
-             "Claude-3-Sonnet", "Claude-3-Haiku", "Llama-3.1-405B-T", "Llama-3.1-405B-FW-128k", "Llama-3.1-70B-T", "Llama-3.1-70B-FW-128k", "Llama-3-70b-Groq", "Gemini-1.5-Pro", "Gemini-1.5-Pro-128k",
-             "Gemini-1.5-Pro-1M", "DALL-E-3", "StableDiffusionXL"}
+# 获取toml配置文件中的bot字典
+bot_config_dict = config['bot']
 
+# 自动生成bot_names字典
+bot_names = {value: value for value in bot_config_dict.values()}
+logging.info(bot_names)
+# 检查bot_names中是否有"Assistant"，如果没有则添加
+if "Assistant" not in bot_names:
+    bot_names["Assistant"] = "Assistant"
+
+# 打印结果以验证
 
 async def get_responses(api_key, prompt, bot):
     if bot in bot_names:
